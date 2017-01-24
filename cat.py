9f6369202dd15d1d1c374bdedc4756854c97b3e2
Pandu POLUAN <pepoluan@gmail.com>
2017-01-25 03:43:31 +0700

    Conversion to generator
    Also removal of unused internal lists.

--- cat.py
+++ cat.py
@@ -7,21 +7,21 @@ import fnmatch
 
 # noinspection PyShadowingBuiltins
 def file(name, indent=0):
-    ind, ret = ' ' * indent, []
+    ind = ' ' * indent
     with open(name, 'rt') as fin:
         for ln in fin:
-            ret.append('{0}{1}'.format(ind, ln.rstrip('\r\n')))
-    return ret
+            yield '{0}{1}'.format(ind, ln.rstrip('\r\n'))
+    return
 
 
 def directory(name, recurse=False, indent=0, file_filter='*', dir_filter='*'):
-    ret = []
     for root, dirs, files in os.walk(name):
         root_base = os.path.basename(root)
         if fnmatch.fnmatch(root_base, dir_filter):
             for fn in files:
                 if fnmatch.fnmatch(fn, file_filter):
-                    ret.extend(file(os.path.join(root, fn), indent=indent))
+                    for ln in file(os.path.join(root, fn), indent=indent):
+                        yield ln
         if not recurse:
             break
-    return ret
+    return
